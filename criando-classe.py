51ae1293a564480780129431f24482eea6789b06
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-25 15:54:59 -0300

    Update criando-classe.py

--- criando-classe.py
+++ criando-classe.py
@@ -1,10 +1,10 @@
 class Pessoa:
-    def __init__ (self, nome, idade, altura, peso):
+    def __init__ (self, nome, data_nasc, altura, peso):
         self.nome = nome
-        self.idade = idade
+        self.data_nasc = data_nasc
         self.altura = altura
         self.peso = peso 
 
-p1 = Pessoa("RC",52,180,89.5)
+p1 = Pessoa("RC","13/05/50",1.70,89.5)
 
-print(p1.nome, p1.idade, p1.altura)
+print(p1.nome, p1.altura, "m   ", p1.data_nasc)
