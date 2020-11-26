df6f8b8e9643c691969d96bacc7d310748a7dea2
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-26 11:24:07 -0300

    26/11

---
+++ fibonacci.php
@@ -0,0 +1,11 @@
+function fibonacci($n)
+{
+    $sequence = [0, 1];
+
+    for ($i = 2; $i < $n; $i++) {
+        $sequence[$i] = $sequence[$i-1] + $sequence[$i-2];
+    }
+
+    return $sequence;
+}
+fibonacci(6); // [0, 1, 1, 2, 3, 5]
