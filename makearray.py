7ee7a18d3098088cdd4e76d61e532a92c531eb92
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-28 21:38:01 -0300

    Create makearray.py

---
+++ makearray.py
@@ -0,0 +1,8 @@
+from datascience import *
+import numpy as np
+
+# %matplotlib inline
+import matplotlib.pyplot as plots
+plots.style.use('fivethirtyeight')
+make_array(1, 2, 3)
+movies.scatter('Year', 'Growth rate')
